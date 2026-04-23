b7209f305d0f85ca8e0b58168ff63391c8eb4432
laporejero <laporejero1297@gmail.com>
2026-04-23 13:41:26 +0800

    practicing file-base navigation

--- app/about.jsx
+++ app/about.jsx
@@ -1,23 +1,17 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import { Link } from 'expo-router'
-import Logo from '../assets/img/logo_light.png'
 
-const Home = () => {
+const About = () => {
     return (
         <View style={styles.container}>
-            <Image source={Logo} style={styles.img} />
-            <Text style={styles.title}>The Number 1</Text>
-            <Text style={{ marginTop: 10, marginBottom: 30 }}>
-                Reading List App
-            </Text>
-            <Link href="/about" style={styles.link}>About Page</Link>
-            <Link href="/contact" style={styles.link}>Contact Page</Link>
+            <Text style={styles.title}>About Page</Text>
+
+            <Link href="/" style={styles.link}>Back Home</Link>
         </View>
     )
 }
 
-export default Home
-
+export default About
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -29,7 +23,7 @@ const styles = StyleSheet.create({
     },
     title: {
         fontWeight: 'bold',
-        fontSize: 18
+        fontSize: 18,
     },
     link: {
         marginVertical: 10,
