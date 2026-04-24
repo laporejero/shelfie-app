1c4267f889d66c0e0a35c822f6bf1bd8ea3123d4
laporejero <laporejero1297@gmail.com>
2026-04-24 15:23:00 +0800

    initial auth state update

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import { account } from "../lib/appwrite"
 import { ID } from "appwrite"
 
@@ -6,6 +6,7 @@ export const UserContext = createContext()
 
 export function UserProvider({ children }) {
     const [user, setUser] = useState(null)
+    const [authChecked, setAuthChecked] = useState(false)
 
     async function login(email, password) {
         try {
@@ -31,8 +32,23 @@ export function UserProvider({ children }) {
         setUser(null)
     }
 
+    async function getInitialUserValue() {
+        try {
+            const response = await account.get()
+            setUser(response)
+        } catch (error) {
+            setUser(null)
+        } finally {
+            setAuthChecked(true)
+        }
+    }
+
+    useEffect(() => {
+        getInitialUserValue()
+    }, [])
+
     return (
-        <UserContext.Provider value={{ user, login, register, logout }}>
+        <UserContext.Provider value={{ user, login, register, logout, authChecked }}>
             {children}
         </UserContext.Provider>
     )
