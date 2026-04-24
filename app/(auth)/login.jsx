11bd485ff65dd8b526f8054bba0c49925696b5f2
laporejero <laporejero1297@gmail.com>
2026-04-24 14:50:47 +0800

    showing error messages update

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -14,14 +14,17 @@ import ThemedTextInput from '../../components/ThemedTextInput'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
 
   const { login } = useUser()
 
   const handleSubmit = async () => {
+    setError(null)
+
     try {
         await login(email, password)
     } catch (error) {
-
+        setError(error.message)
     }
   }
 
@@ -55,6 +58,9 @@ const Login = () => {
             <ThemedButton onPress={handleSubmit}>
                 <Text style={{ color: '#f2f2f2' }}>Login</Text>
             </ThemedButton>
+
+            <Spacer />
+            {error && <Text style={styles.error}>{error}</Text>}
 
             <Spacer height={100} />
 
@@ -90,5 +96,14 @@ const styles = StyleSheet.create({
     },
     pressed: {
         opacity: 0.8
+    },
+    error: {
+        color: Colors.warning,
+        padding: 10,
+        backgroundColor: '#f5c1c8',
+        borderColor: Colors.warning,
+        borderWidth: 1,
+        borderRadius: 6,
+        marginHorizontal: 10,
     }
 })
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,5 +1,6 @@
 import { Keyboard, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 import { useState } from 'react'
 import { useUser } from '../../hooks/useUser'
 
@@ -13,14 +14,17 @@ import ThemedTextInput from '../../components/ThemedTextInput'
 const Register = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
 
   const { register } = useUser()
 
   const handleSubmit = async () => {
+    setError(null)
+
     try {
         await register(email, password)
     } catch (error) {
-
+        setError(error.message)
     }
   }
 
@@ -55,6 +59,9 @@ const Register = () => {
                 <Text style={{ color: '#f2f2f2' }}>Register</Text>
             </ThemedButton>
 
+            <Spacer />
+            {error && <Text style={styles.error}>{error}</Text>}
+
             <Spacer height={100} />
 
             {/* Login page link */}
@@ -82,4 +89,13 @@ const styles = StyleSheet.create({
         fontSize: 18,
         marginBottom: 30
     },
+    error: {
+        color: Colors.warning,
+        padding: 10,
+        backgroundColor: '#f5c1c8',
+        borderColor: Colors.warning,
+        borderWidth: 1,
+        borderRadius: 6,
+        marginHorizontal: 10,
+    }
 })
